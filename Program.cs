08ca0199c252f754c691e4799f60a69f7326d0cb
Pedro Ivo Maldonado Roschel <134627957+pivo139@users.noreply.github.com>
2024-05-14 21:20:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+  
+double salário;
+  double salárioreajustado;
+    
+Console.Write("Informe o seu salário: ");
+  salário = double.Parse(Console.ReadLine());
+
+    if(salário > 1700){
+      salárioreajustado = (salário * 0.20) + salário;
+    }
+
+    else{
+      salárioreajustado = (salário * 0.30) + salário;
+    }
+
+    Console.WriteLine(" O salário reajustado é de R$: {0:n2}", salárioreajustado);
+  
+  } 
+}
